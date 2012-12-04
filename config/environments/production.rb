43dc66741447fc8009b589df229fba657a64f96e
Joy Kim <jojo080889@gmail.com>
2012-12-03 19:14:39 -0800

    space

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Uploader::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-
+ 
   # Code is not reloaded between requests
   config.cache_classes = true
 
